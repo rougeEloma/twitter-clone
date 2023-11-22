de536ec2125bb34ac809cb8b828d6ccd45533df7
alain eloma <alainelomapro@gmail.com>
2023-11-22 12:40:29 +0100

    header rendu on-going

--- src/components/header-cpts/tweetEditorButtons.jsx
+++ src/components/header-cpts/tweetEditorButtons.jsx
@@ -3,7 +3,7 @@ import TweetEditorActions from "./tweetEditorActions";
 import TweetButton from "./tweetButton";
 
 function TweetEditorButtons () {
-    <div className="tweet-editor-action">
+    <div className="tweet-editor-buttons">
         <TweetEditorActions />
         <TweetButton />
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/header-cpts/header';
+import TweetEditor from '../components/header-cpts/tweetEditor';
 
 function Home() {
   return (
     <main className="timeline">
       <Header />
+      <TweetEditor />
     </main>
   );
 }
