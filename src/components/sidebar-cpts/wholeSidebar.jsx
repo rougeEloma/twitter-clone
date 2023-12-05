61a675b8ef331292d0ba6f6075887afff9307474
alain eloma <alainelomapro@gmail.com>
2023-12-05 11:30:52 +0100

    sidebar done... for now

--- src/components/sidebar-cpts/wholeSidebar.jsx
+++ src/components/sidebar-cpts/wholeSidebar.jsx
@@ -1,21 +1,41 @@
 import roundTwitterLogo from "../../images/round-twitter-logo.svg"
+import avatarImg from "../../images/profile-photo.png"
+import dotsMenu from "../../images/dotsMenu.svg"
 
 function Sidebar () {
     return (
-        <>
-        <img src={roundTwitterLogo} alt="" className='tweet-avatar'/>
-        <ul>
-          <li>Acceuil</li>
-          <li>Explorer</li>
-          <li>Notifications</li>
-          <li>Messages</li>
-          <li>Bookmarks</li>
-          <li>Lists</li>
-          <li><a>Profile</a></li>
-          <li>More</li>
-        </ul>
-        <button className='side-tweet-button'>Tweet</button>
-        </>
+        <div className="positioning">
+
+          {/* side-twitter-logo component */}
+          <img src={roundTwitterLogo} alt="" className='tweet-avatar'/>
+          <ul>
+            <li>Acceuil</li>
+            <li>Explorer</li>
+            <li>Notifications</li>
+            <li>Messages</li>
+            <li>Bookmarks</li>
+            <li>Lists</li>
+            <li><a>Profile</a></li>
+            <li>More</li>
+          </ul>
+
+          {/* side-Tweet-button component */}
+          <button className='side-tweet-button'>Tweet</button>
+
+          {/* side-user-infos component */}
+          <div className="side-user-info">
+            <div className="profile-picture">
+                <img src={avatarImg} alt="" />
+            </div>
+            <div className="user-ids">
+                <h2 className="tweet-title-author">The X</h2>
+                <p className="tweet-title-details">@ElonMusk</p>
+            </div>
+            <div className="more-info">
+                <img src={dotsMenu} alt="" />
+            </div>
+        </div>
+        </div>
     )
 }
 
