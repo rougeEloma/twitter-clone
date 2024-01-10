930fe2e11d221431dff04f014beabaa9d647994b
alain eloma <alainelomapro@gmail.com>
2024-01-10 11:57:08 +0100

    profile page - created and set

--- src/components/profiles-cpts/ProfileHeader.jsx
+++ src/components/profiles-cpts/ProfileHeader.jsx
@@ -1,9 +1,15 @@
-
+import EachProfile from "../profilesDatas";
 
 function ProfileHeader () {
     return(
         <>
-        <div>profile Header</div>
+        <div className="header">Profile's name</div>
+        <div className="header-photo"><img src="" alt="" /></div>
+        <div className="avatar"><img src="" alt="" /></div>
+        <div>name</div>
+        <div>@username</div>
+        <div className="infos">everywhere about.x.comJoined February 2007</div>
+        <div className="follow">0 Following || 67.3M Followers</div>
         </>
     )
 }
